0f507c80ea26e1c6e42a165b216f0c4764398c4c
IamCathal <cathalsocallaghan@gmail.com>
2023-02-04 15:34:36 +0000

    Get and render problems on dashboard

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,4 +1,5 @@
 getAndRenderTeamsData()
+getAndRenderProblems()
 initWsTeamUpdateListener()
 
 function initWsTeamUpdateListener() {
@@ -168,9 +169,71 @@ function clearInputsForCreateTeam() {
 function getAndRenderTeamsData() {
     getScoreboardData().then((teams) => {
         renderTeamEditRows(teams)
-    }, (error) => {
-        console.error(error)
+    }, (err) => {
+        console.error(err)
     })
+}
+
+function getAndRenderProblems() {
+    getProblems().then(problems => {
+        renderProblems(problems)
+    }, (err) => {
+        console.error(err)
+    })
+}
+
+function renderProblems(problems) {
+    let output = ``
+
+    problems.forEach(problem => {
+
+        output += `
+        
+    <div class="row">
+        <div class="col">
+            <div class="row pt-1">
+                <div class="col-9">
+                    <textarea 
+                        id="${problem.problemId}-problemText"
+                        class="teamInput"
+                        contenteditable="true"
+                        style="width: 95%"
+                    >
+                    ${problem.problemText}
+                    </textarea>
+                </div>
+                <div class="col-3 scoreboardLegendText">
+                    <div class="row pb-1">
+                        <div class="col">
+                            <textarea 
+                                id="${problem.problemId}-answer"
+                                class="teamInput teamMemberInput"
+                                contenteditable="true"
+                                style="width: 95%"
+                            >
+                            ${problem.problemAnswer}
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row pt-2 pb-3">
+                <div class="col-2 text-center">
+                    <button class="createProblemButton" id="createTeam-createTeamButton">
+                        Update Problem
+                    </button>
+                </div>
+                <div class="col" id="${problem.problemId}}-requestInfoBox">
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+    })
+    document.getElementById("problemsEditRow").innerHTML = ""
+    document.getElementById("problemsEditRow").innerHTML += output;
 }
 
 function updateTeam(teamID, updatedName, updatedMembers, updatedScore) {
@@ -253,6 +316,23 @@ function createTeam(teamName, teamMembers, newTeamScore) {
             }
         }, (err) => {
             reject(err.json())
+        });
+    })
+}
+
+function getProblems() {
+    return new Promise((resolve, reject) => {
+        fetch(`/getproblems`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+        }).then((res) => res.json())
+        .then((res) => {
+            resolve(res)
+        }, (err) => {
+            reject(err)
         });
     })
 }
